08d5d759ab1fe76987ed88087e4c3d3cb6fbe8e6
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-08-03 22:08:52 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,41 +1,18 @@
 ﻿using System;
 
-namespace Circle
+namespace Lab10
 {
     class Program
     {
-        public double Radius { get; set; }
-        public double Circumference { get; set; }
-
-        public double Area { get; set; }
-
-        public Circle(double radius)
+        static void Main(string[] args)
         {
-            Radius = radius;
-        }
-        public double CalculateCircumference()
-        {
-            double Circumference = 2 * Math.PI * Radius;
-        }
-
-        public double CalculateFormattedCircumference()
-        {
-            Math.Round(Circumference, 2);
-        }
-
-        public double CalculateArea()
-        {
-            double Area = Math.PI * (Radius * Radius);
-        }
-
-        public double CalculateFormattedArea()
-        {
-            Math.Round(Area, 2);
-        }
-
-        public double FormatNumber(double x)
-        {
-            //???
+            Console.WriteLine("Welcome to the Circle Tester!");
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double circumference = 2 * Math.PI * radius;
+            double area = Math.PI * (radius * radius);
+            Console.WriteLine("Circumference: " + circumference);
+            Console.WriteLine("Area: " + area);
         }
     }
 }
